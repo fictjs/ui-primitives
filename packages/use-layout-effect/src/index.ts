1060c51803b60c2fb8ec6c3b66dab7c333fc68f7
unadlib <unadlib@gmail.com>
2026-03-10 20:17:29 +0800

    fix: defer use-layout-effect registration until mount

--- packages/use-layout-effect/src/index.ts
+++ packages/use-layout-effect/src/index.ts
@@ -1,13 +1,13 @@
-import { type Cleanup } from '@fictjs/runtime'
-import { __fictUseContext, __fictUseEffect } from '@fictjs/runtime/internal'
+import { createEffect, onMount, type Cleanup } from '@fictjs/runtime'
 
 function useLayoutEffect(effect: () => void | Cleanup): void {
   if (typeof document === 'undefined') {
     return
   }
 
-  const ctx = __fictUseContext()
-  __fictUseEffect(ctx, effect)
+  onMount(() => {
+    return createEffect(effect)
+  })
 }
 
 export { useLayoutEffect }
