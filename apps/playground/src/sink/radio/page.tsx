8ad566a33172212f1eae277f073221b28c51c9c8
unadlib <unadlib@gmail.com>
2026-03-17 20:58:34 +0800

    refactor: remove radio display-contents wrappers

--- apps/playground/src/sink/radio/page.tsx
+++ apps/playground/src/sink/radio/page.tsx
@@ -2,8 +2,106 @@ import { Radio, Grid, Flex, Text, Code, Box, Separator, Table } from '@fictjs/ra
 import { radioPropDefs } from '@fictjs/radix-ui-themes/props';
 import { DocsSection, DocsSectionBody, DocsSectionHeading } from '../docs-section';
 import { accentColorsGrouped } from '../_utils';
+import type { FictNode } from 'fict';
 
 export default function RadioPage() {
+  const stateMatrixRows: FictNode[] = [];
+  const colorCombinationContent: FictNode[] = [];
+
+  for (const variant of radioPropDefs.variant.values) {
+    for (const label of [variant, '+ high-contrast'] as const) {
+      stateMatrixRows.push(
+        <Table.Row key={`${variant}-${label}`}>
+          <Table.RowHeaderCell>{label}</Table.RowHeaderCell>
+          <Table.Cell>
+            <Radio variant={variant} highContrast={label === '+ high-contrast'} value="value" />
+          </Table.Cell>
+          <Table.Cell>
+            <Radio
+              checked
+              variant={variant}
+              highContrast={label === '+ high-contrast'}
+              value="value"
+            />
+          </Table.Cell>
+          <Table.Cell>
+            <Radio
+              variant={variant}
+              highContrast={label === '+ high-contrast'}
+              value="value"
+              disabled
+            />
+          </Table.Cell>
+          <Table.Cell>
+            <Radio
+              checked
+              variant={variant}
+              highContrast={label === '+ high-contrast'}
+              disabled
+              value="value"
+            />
+          </Table.Cell>
+        </Table.Row>,
+      );
+    }
+  }
+
+  for (const group of accentColorsGrouped) {
+    colorCombinationContent.push(
+      <Text key={`${group.label}-heading`} as="p" weight="bold" mt="6" mb="4">
+        {group.label}
+      </Text>,
+    );
+    colorCombinationContent.push(
+      <Table.Root key={`${group.label}-table`}>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell />
+            {radioPropDefs.variant.values.map((variant) => (
+              <Table.ColumnHeaderCell key={variant}>{variant}</Table.ColumnHeaderCell>
+            ))}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {group.values.map((color) => (
+            <Table.Row key={color}>
+              <Table.RowHeaderCell>{color}</Table.RowHeaderCell>
+              {radioPropDefs.variant.values.map((variant) => (
+                <Table.Cell key={variant}>
+                  <Flex gap="2">
+                    <Radio color={color} variant={variant} name={`radio-${variant}-${color}`} value="1" />
+                    <Radio
+                      color={color}
+                      variant={variant}
+                      name={`radio-${variant}-${color}`}
+                      value="2"
+                      defaultChecked
+                    />
+                    <Radio
+                      color={color}
+                      variant={variant}
+                      name={`radio-${variant}-${color}-high-contrast`}
+                      highContrast
+                      value="1"
+                    />
+                    <Radio
+                      color={color}
+                      variant={variant}
+                      name={`radio-${variant}-${color}-high-contrast`}
+                      highContrast
+                      value="2"
+                      defaultChecked
+                    />
+                  </Flex>
+                </Table.Cell>
+              ))}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>,
+    );
+  }
+
   return (
     <DocsSection>
       <DocsSectionHeading>Radio</DocsSectionHeading>
@@ -20,49 +118,7 @@ export default function RadioPage() {
                   <Table.ColumnHeaderCell>disabled checked</Table.ColumnHeaderCell>
                 </Table.Row>
               </Table.Header>
-              <Table.Body>
-                {radioPropDefs.variant.values.map((variant) => (
-                  <div key={variant} style={{ display: 'contents' }}>
-                    {[variant, '+ high-contrast'].map((label) => (
-                      <Table.Row key={label}>
-                        <Table.RowHeaderCell>{label}</Table.RowHeaderCell>
-                        <Table.Cell>
-                          <Radio
-                            variant={variant}
-                            highContrast={label === '+ high-contrast'}
-                            value="value"
-                          />
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Radio
-                            checked
-                            variant={variant}
-                            highContrast={label === '+ high-contrast'}
-                            value="value"
-                          />
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Radio
-                            variant={variant}
-                            highContrast={label === '+ high-contrast'}
-                            value="value"
-                            disabled
-                          />
-                        </Table.Cell>
-                        <Table.Cell>
-                          <Radio
-                            checked
-                            variant={variant}
-                            highContrast={label === '+ high-contrast'}
-                            disabled
-                            value="value"
-                          />
-                        </Table.Cell>
-                      </Table.Row>
-                    ))}
-                  </div>
-                ))}
-              </Table.Body>
+              <Table.Body>{stateMatrixRows}</Table.Body>
             </Table.Root>
 
             <Table.Root>
@@ -212,64 +268,7 @@ export default function RadioPage() {
               See colors & variants combinations
             </Text>
           </summary>
-          {accentColorsGrouped.map(({ label, values }) => (
-            <div key={label} style={{ display: 'contents' }}>
-              <Text as="p" weight="bold" mt="6" mb="4">
-                {label}
-              </Text>
-              <Table.Root>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.ColumnHeaderCell />
-                    {radioPropDefs.variant.values.map((variant) => (
-                      <Table.ColumnHeaderCell key={variant}>{variant}</Table.ColumnHeaderCell>
-                    ))}
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {values.map((color) => (
-                    <Table.Row key={color}>
-                      <Table.RowHeaderCell>{color}</Table.RowHeaderCell>
-                      {radioPropDefs.variant.values.map((variant) => (
-                        <Table.Cell key={variant}>
-                          <Flex gap="2">
-                            <Radio
-                              color={color}
-                              variant={variant}
-                              name={`radio-${variant}-${color}`}
-                              value="1"
-                            />
-                            <Radio
-                              color={color}
-                              variant={variant}
-                              name={`radio-${variant}-${color}`}
-                              value="2"
-                              defaultChecked
-                            />
-                            <Radio
-                              color={color}
-                              variant={variant}
-                              name={`radio-${variant}-${color}-high-contrast`}
-                              highContrast
-                              value="1"
-                            />
-                            <Radio
-                              color={color}
-                              variant={variant}
-                              name={`radio-${variant}-${color}-high-contrast`}
-                              highContrast
-                              value="2"
-                              defaultChecked
-                            />
-                          </Flex>
-                        </Table.Cell>
-                      ))}
-                    </Table.Row>
-                  ))}
-                </Table.Body>
-              </Table.Root>
-            </div>
-          ))}
+          {colorCombinationContent}
         </details>
 
         <Separator size="3" my="5" />
