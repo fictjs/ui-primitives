a4db1ab4ef5f6e7af4b9010ec23e596317f4dd84
unadlib <unadlib@gmail.com>
2026-07-10 19:33:47 +0800

    chore(format): check staged stylesheets

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
   '*.{js,mjs,cjs,ts,mts,cts}': ['eslint --fix --max-warnings=0', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
+  '*.{css,json,md,yml,yaml}': ['prettier --write'],
 }
